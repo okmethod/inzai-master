2e043536bfc3125ec6f70c2c0fdf216a6839d5c3
okugi <83124702+okmethod@users.noreply.github.com>
2024-10-05 08:41:55 +0900

    Initial commit

---
+++ skeleton-app/src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import "../app.postcss";
+  import { AppBar } from "@skeletonlabs/skeleton";
+  import { Toast, Modal, initializeStores } from "@skeletonlabs/skeleton";
+  initializeStores();
+
+  // Floating UI for Popups
+  import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
+  import { storePopup } from "@skeletonlabs/skeleton";
+  storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+  import { navigateTo } from "$lib/utils/navigation.client";
+</script>
+
+<svelte:head>
+  <title>My Static WebSite</title>
+</svelte:head>
+
+<Modal />
+<Toast position="tr" rounded="rounded-lg" />
+
+<div class="flex flex-col h-screen">
+  <div class="border-b border-gray-400">
+    <AppBar class="!p-1 md:!p-2">
+      <div class="flex items-center h-full">
+        <a href="/" class="flex items-center" on:click|preventDefault={() => navigateTo("/")}>
+          <div class="text-sm md:text-lg">TOP</div>
+        </a>
+      </div>
+    </AppBar>
+  </div>
+
+  <div class="container mx-auto overflow-y-auto pb-16">
+    <slot />
+  </div>
+</div>
