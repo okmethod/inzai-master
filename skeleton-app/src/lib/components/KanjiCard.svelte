a256564c5825c616ed0eb53a209a701d62db7ebf
shohei.okugi <shohei.okugi@ghelia.com>
2024-10-05 11:08:28 +0900

    :sparkles: show inzai KanjiCard

---
+++ skeleton-app/src/lib/components/KanjiCard.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { diffChars } from "diff";
+  import type { kanjiCsv } from "$lib/types/csv";
+
+  export let data: kanjiCsv;
+  export let kanjiMode = false;
+  export let showAnswer = false;
+
+  interface DiffPart {
+    str: string;
+    isDiff: boolean;
+    isKanji: boolean;
+  }
+
+  function getDiffParts(kanji: string, yomi: string): DiffPart[] {
+    const diff = diffChars(kanji, yomi);
+    return diff.map((part) => ({
+      str: part.value,
+      isDiff: !!part.added || !!part.removed,
+      isKanji: !!part.removed,
+    }));
+  }
+
+  const diffParts = getDiffParts(data.kanji, data.yomi);
+</script>
+
+<div class="border rounded bg-yellow-100 min-w-80 p-2 space-y-1">
+  <h2 class="text-xl">
+    {#each diffParts as part}
+      {#if part.isDiff}
+        {#if part.isKanji === kanjiMode}
+          <span class="font-bold underline">{part.str}</span>
+        {/if}
+      {:else}
+        {part.str}
+      {/if}
+    {/each}
+  </h2>
+  <p class="mx-2">
+    {#if showAnswer}
+      {#each diffParts as part}
+        {#if part.isDiff}
+          {#if part.isKanji !== kanjiMode}
+            <span>{part.str}</span>
+          {/if}
+        {/if}
+      {/each}
+    {:else}
+      <button on:click={() => (showAnswer = true)}>
+        <span class="border rounded bg-gray-100 p-1">正解を見る</span>
+      </button>
+    {/if}
+  </p>
+</div>
--- skeleton-app/src/routes/kanji/+page.svelte
+++ skeleton-app/src/routes/kanji/+page.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import type { kanjiCsv } from "$lib/types/csv";
+  import KanjiCard from "$lib/components/KanjiCard.svelte";
+  import { loadCsv } from "$lib/utils/loadfile";
+
+  let inzai: kanjiCsv[] = [];
+  onMount(async () => {
+    inzai = (await loadCsv(window.fetch, "kanji/inzai.csv")) as kanjiCsv[];
+  });
+
+  let kanjiMode = true;
+  function toggleMode() {
+    kanjiMode = !kanjiMode;
+  }
 </script>
 
 <div class="cRouteBodyStyle">
@@ -9,6 +23,14 @@
 
   <!-- コンテンツ部 -->
   <div class="cContentPartStyle !m-4">
-    <span>hoge fuga</span>
+    <button on:click={toggleMode}>
+      <span class="border rounded bg-gray-100 p-1">{kanjiMode ? "漢字モード" : "読みモード"}</span>
+    </button>
+    <h2 class="text-xl font-bold underline">印西の漢字</h2>
+    <div class="grid grid-cols-3 gap-2">
+      {#each inzai as row}
+        <KanjiCard data={row} {kanjiMode} showAnswer={false} />
+      {/each}
+    </div>
   </div>
 </div>
