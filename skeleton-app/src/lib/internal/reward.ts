bf392cdda2a80fc0cc85cc5efe61476ad054d513
shohei.okugi <shohei.okugi@ghelia.com>
2024-10-13 19:13:38 +0900

    :recycle: put out getJSTDateString

--- skeleton-app/src/lib/internal/reward.ts
+++ skeleton-app/src/lib/internal/reward.ts
@@ -1,17 +1,4 @@
-function getJSTDateString(date: Date): string {
-  const options: Intl.DateTimeFormatOptions = {
-    timeZone: "Asia/Tokyo",
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  };
-  const formatter = new Intl.DateTimeFormat("ja-JP", options);
-  const parts = formatter.formatToParts(date);
-  const year = parts.find((part) => part.type === "year")?.value;
-  const month = parts.find((part) => part.type === "month")?.value;
-  const day = parts.find((part) => part.type === "day")?.value;
-  return `${year}-${month}-${day}`;
-}
+import { getJSTDateString } from "$lib/utils/dateString";
 
 export function rewardDailyLogin(latestLoginRewardDate: Date): number {
   const now = new Date();
---
+++ skeleton-app/src/lib/utils/dateString.ts
@@ -0,0 +1,14 @@
+export function getJSTDateString(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: "Asia/Tokyo",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  };
+  const formatter = new Intl.DateTimeFormat("ja-JP", options);
+  const parts = formatter.formatToParts(date);
+  const year = parts.find((part) => part.type === "year")?.value;
+  const month = parts.find((part) => part.type === "month")?.value;
+  const day = parts.find((part) => part.type === "day")?.value;
+  return `${year}-${month}-${day}`;
+}
