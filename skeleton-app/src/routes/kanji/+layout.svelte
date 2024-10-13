df54e49b57bab1b0ca6ef5d3ed2feb14ff51f481
shohei.okugi <shohei.okugi@ghelia.com>
2024-10-13 21:31:48 +0900

    :+1: disable selectKanjiMode at exam page

--- skeleton-app/src/routes/kanji/+layout.svelte
+++ skeleton-app/src/routes/kanji/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { setContext } from "svelte";
-  import { writable } from "svelte/store";
+  import { writable, derived } from "svelte/store";
+  import { page } from "$app/stores";
   import type { SlotTabSetting } from "$lib/types/tabSetting";
   import type { KanjiMode } from "$lib/types/kanji";
   import ContentsTabGroup from "$lib/components/ContentsTabGroup.svelte";
@@ -12,13 +13,24 @@
 
   let currentTabIndex = 0;
 
-  let currentMode: KanjiMode = "yomi";
-  const modeStore = writable<KanjiMode>(currentMode);
+  const modeStore = writable<KanjiMode>("yomi");
   setContext("mode", modeStore);
   function updateKanjiMode(mode: KanjiMode) {
-    currentMode = mode;
-    modeStore.set(currentMode);
+    modeStore.set(mode);
   }
+
+  function getButtonClass(target: KanjiMode, mode: KanjiMode, disabled: boolean): string {
+    let baseClass = "cButtonYellowStyle";
+    if (mode === target) {
+      baseClass += " !bg-yellow-900";
+    }
+    if (disabled) {
+      baseClass += " !bg-gray-500";
+    }
+    return baseClass;
+  }
+
+  const isExamPath = derived(page, ($page) => $page.url.pathname === "/kanji/exam");
 </script>
 
 <div class="cRouteBodyStyle">
@@ -27,12 +39,12 @@
     <h1 class="cTitleStyle md:!text-3xl">漢字マスタードリル</h1>
     <div class="mx-4 mt-1 md:mt-0">
       <span class="text-gray-500 hidden md:inline md:mx-3">:</span>
-      <button on:click={() => updateKanjiMode("yomi")}>
-        <span class="cButtonYellowStyle {currentMode === 'yomi' ? '!bg-yellow-900' : ''}">読み問題</span>
+      <button on:click={() => updateKanjiMode("yomi")} disabled={$isExamPath}>
+        <span class={getButtonClass("yomi", $modeStore, $isExamPath)}>読み問題</span>
       </button>
       <span class="mx-3 text-gray-500">|</span>
-      <button on:click={() => updateKanjiMode("kaki")}>
-        <span class="cButtonYellowStyle {currentMode === 'kaki' ? '!bg-yellow-900' : ''}">書き問題</span>
+      <button on:click={() => updateKanjiMode("kaki")} disabled={$isExamPath}>
+        <span class={getButtonClass("kaki", $modeStore, $isExamPath)}>書き問題</span>
       </button>
     </div>
   </div>
