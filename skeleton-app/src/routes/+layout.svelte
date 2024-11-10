4b28c3f7c685fd5c77c9098d1165dd01f6cfdf12
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-10 22:29:25 +0900

    :+1: apply theme

--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -13,6 +13,18 @@
   import type { User } from "@auth0/auth0-spa-js";
   import UserButton from "$lib/components/UserButton.svelte";
 
+  import { onMount } from "svelte";
+  import { applyTheme } from "$lib/stores/theme";
+
+  let isLoaded = false;
+  onMount(async () => {
+    function wait(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    await Promise.all([applyTheme(), wait(500)]);
+    isLoaded = true;
+  });
+
   export let data: {
     user: User | null;
   };
@@ -25,24 +37,30 @@
 <Modal />
 <Toast position="tr" rounded="rounded-lg" />
 
-<div class="flex flex-col h-screen">
-  <div class="relative border-b border-gray-400 bg-gray-100">
-    <div class="flex items-center justify-between h-full">
-      <button
-        class="cButtonGrayStyle flex flex-row items-center space-x-1 m-1"
-        on:click|preventDefault={() => goto("/")}
-      >
-        <div class="w-5 h-5">
-          <Icon icon="mdi:home-outline" class="w-full h-full" />
-        </div>
-        <span class="">HOME</span>
-      </button>
-      <div class="flex-grow"><!--spacer--></div>
-      <UserButton user={data.user} />
+{#if isLoaded}
+  <div class="h-screen flex flex-col">
+    <div class="relative border-b border-gray-400 bg-gray-100">
+      <div class="h-full flex items-center justify-between">
+        <button
+          class="cButtonGrayStyle flex flex-row items-center space-x-1 m-1"
+          on:click|preventDefault={() => goto("/")}
+        >
+          <div class="w-5 h-5">
+            <Icon icon="mdi:home-outline" class="w-full h-full" />
+          </div>
+          <span class="">HOME</span>
+        </button>
+        <div class="flex-grow"><!--spacer--></div>
+        <UserButton user={data.user} />
+      </div>
+    </div>
+
+    <div class="w-screen mx-auto overflow-y-scroll scrollbar-gutter-stable sm:ml-2 pb-24">
+      <slot />
     </div>
   </div>
-
-  <div class="container mx-auto overflow-y-auto pb-2">
-    <slot />
+{:else}
+  <div class="h-screen flex items-center justify-center bg-gray-100">
+    <div class="font-mono text-black text-2xl">Now Loading...</div>
   </div>
-</div>
+{/if}
