5b06654dc74fb348c8b651205914c02e3da95239
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-13 23:32:34 +0900

    :+1: use SlideToggle instead of cButtonYellowStyle

--- skeleton-app/src/routes/kanji/+layout.svelte
+++ skeleton-app/src/routes/kanji/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { setContext } from "svelte";
   import { writable, derived } from "svelte/store";
+  import { SlideToggle } from "@skeletonlabs/skeleton";
   import { page } from "$app/stores";
   import type { SlotTabSetting } from "$lib/types/tabSetting";
   import type { KanjiMode } from "$lib/types/kanji";
@@ -13,42 +14,34 @@
 
   let currentTabIndex = 0;
 
+  let isKakiMode = false;
   const modeStore = writable<KanjiMode>("yomi");
   setContext("mode", modeStore);
-  function updateKanjiMode(mode: KanjiMode) {
+  function updateKanjiMode() {
+    const mode = isKakiMode ? "kaki" : "yomi";
     modeStore.set(mode);
-  }
-
-  function getButtonClass(target: KanjiMode, mode: KanjiMode, disabled: boolean): string {
-    let baseClass = "cButtonYellowStyle";
-    if (disabled) {
-      baseClass += " !bg-gray-500";
-    } else if (mode === target) {
-      baseClass += " !bg-yellow-900";
-    }
-    return baseClass;
   }
 
   const isExamPath = derived(page, ($page) => $page.url.pathname === "/kanji/exam");
 </script>
 
 <div class="cRouteBodyStyle">
-  <!-- タイトル部 -->
   <div class="cTitlePartStyle flex flex-col md:flex-row justify-center items-center">
     <h1 class="cTitleStyle md:!text-3xl">漢字マスタードリル</h1>
-    <div class="mx-4 mt-1 md:mt-0">
-      <span class="text-gray-500 hidden md:inline md:mx-3">:</span>
-      <button on:click={() => updateKanjiMode("yomi")} disabled={$isExamPath}>
-        <span class={getButtonClass("yomi", $modeStore, $isExamPath)}>読み問題</span>
-      </button>
-      <span class="mx-3 text-gray-500">|</span>
-      <button on:click={() => updateKanjiMode("kaki")} disabled={$isExamPath}>
-        <span class={getButtonClass("kaki", $modeStore, $isExamPath)}>書き問題</span>
-      </button>
-    </div>
+    <span class="text-gray-500 hidden md:inline md:mx-3">:</span>
+    <SlideToggle
+      name="slide"
+      bind:checked={isKakiMode}
+      on:change={updateKanjiMode}
+      background="bg-surface-900 dark:bg-surface-300"
+      active="bg-surface-900 dark:bg-surface-300"
+      class="px-2 py-1"
+      disabled={$isExamPath}
+    >
+      {isKakiMode ? "書き問題モード" : "読み問題モード"}
+    </SlideToggle>
   </div>
 
-  <!-- コンテンツ部 -->
   <ContentsTabGroup
     bind:currentTabIndex
     {tabSettings}
