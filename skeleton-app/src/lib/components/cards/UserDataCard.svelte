a1f812a7cea0777c0322154897d04ea3ef22124c
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-16 22:48:29 +0900

    :recycle: put out UserDataCard

---
+++ skeleton-app/src/lib/components/cards/UserDataCard.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { tweened } from "svelte/motion";
+  import { cubicOut } from "svelte/easing";
+  import { ProgressBar } from "@skeletonlabs/skeleton";
+  import { convertToKanji } from "$lib/utils/numerics";
+
+  export let userImageUrl = "";
+  export let userNickName = "";
+  export let userRewardPoints = 0;
+
+  const rankIndex = Math.floor(userRewardPoints / 100) + 1;
+  const nextRankValue = rankIndex * 100;
+
+  let progressBarValue = tweened(0, {
+    duration: 200, // 200ms
+    easing: cubicOut,
+  });
+  onMount(() => {
+    progressBarValue.set(userRewardPoints);
+  });
+</script>
+
+<div class="w-80 flex flex-col items-center cSurfaceColor border rounded-lg space-y-4 p-4">
+  <img src={userImageUrl} alt="profile" class="w-20 h-20 rounded-full" />
+  <div>{userNickName} さんの自学の成果</div>
+  <div class="flex items-center space-x-1">
+    <span>研鑽ポイント: </span>
+    <span class="inline-block w-10 text-right">{userRewardPoints}</span>
+    <span> pt</span>
+  </div>
+  <div class="flex items-center space-x-1">
+    <span>マスターランク: </span>
+    <span class="inline-block w-14 text-right">{convertToKanji(rankIndex)}</span>
+    <span> 段</span>
+  </div>
+  <ProgressBar
+    value={$progressBarValue}
+    max={nextRankValue}
+    height="h-4"
+    rounded="rounded-full"
+    transition="transition-[width] duration-500"
+    meter="bg-blue-500"
+    track="bg-gray-200"
+  />
+  <div class="flex items-center space-x-1">
+    <span>(次のランクまであと </span>
+    <span class="inline-block w-8 text-right">{nextRankValue - userRewardPoints}</span>
+    <span> pt)</span>
+  </div>
+</div>
--- skeleton-app/src/routes/mypage/+page.svelte
+++ skeleton-app/src/routes/mypage/+page.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { tweened } from "svelte/motion";
-  import { cubicOut } from "svelte/easing";
-  import { ProgressBar } from "@skeletonlabs/skeleton";
   import { getToastStore, getModalStore, type ModalSettings } from "@skeletonlabs/skeleton";
   import type { User } from "@auth0/auth0-spa-js";
   import Auth0Singleton from "$lib/services/Auth0Singleton";
   import { getUser } from "$lib/stores/user";
   import { isEligibleForDailyReward, addRewardPoints, showRewardToast } from "$lib/internal/reward";
-  import { convertToKanji } from "$lib/utils/numerics";
+  import UserDataCard from "$lib/components/cards/UserDataCard.svelte";
   import IconButton from "$lib/components/buttons/IconButton.svelte";
   import ThemeSwitchModal from "$lib/components/modals/ThemeSwitchModal.svelte";
 
@@ -21,8 +17,6 @@
   const userImageUrl = data.user ? data.user.picture : "";
   const userNickName = data.user ? data.user.nickname : "";
   let userRewardPoints = userData ? userData.rewardPoints : 0;
-  const rankIndex = Math.floor(userRewardPoints / 100) + 1;
-  const nextRankValue = rankIndex * 100;
 
   const toastStore = getToastStore();
 
@@ -33,14 +27,6 @@
     showRewardToast(toastStore, rewardKey);
     addedReward = true;
   }
-
-  let progressBarValue = tweened(0, {
-    duration: 200, // 200ms
-    easing: cubicOut,
-  });
-  onMount(() => {
-    progressBarValue.set(userRewardPoints);
-  });
 
   const modalStore = getModalStore();
   function showThemeSwitchModal(): void {
@@ -64,35 +50,7 @@
   <span> マイページを確認するにはログインしてね！ </span>
 {:else}
   <div class="flex flex-col items-center mt-2 space-y-4">
-    <div class="w-80 flex flex-col items-center cSurfaceColor border rounded-lg space-y-4 p-4">
-      <img src={userImageUrl} alt="profile" class="w-20 h-20 rounded-full" />
-      <div>{userNickName} さんの自学の成果</div>
-      <div class="flex items-center space-x-1">
-        <span>研鑽ポイント: </span>
-        <span class="inline-block w-10 text-right">{userRewardPoints}</span>
-        <span> pt</span>
-      </div>
-      <div class="flex items-center space-x-1">
-        <span>マスターランク: </span>
-        <span class="inline-block w-14 text-right">{convertToKanji(rankIndex)}</span>
-        <span> 段</span>
-      </div>
-      <ProgressBar
-        value={$progressBarValue}
-        max={nextRankValue}
-        height="h-4"
-        rounded="rounded-full"
-        transition="transition-[width] duration-500"
-        meter="bg-blue-500"
-        track="bg-gray-200"
-      />
-      <div class="flex items-center space-x-1">
-        <span>(次のランクまであと </span>
-        <span class="inline-block w-8 text-right">{nextRankValue - userRewardPoints}</span>
-        <span> pt)</span>
-      </div>
-    </div>
-
+    <UserDataCard {userImageUrl} {userNickName} {userRewardPoints} />
     <IconButton
       icon="mdi:creation"
       label={addedReward ? "また明日もらえるよ" : "ログインボーナスをもらう"}
